213250a1e7b4d19ce3d27e5464d6fe57beb8d248
Christopher Young <christopheryoung@MacBook123.local>
2022-06-03 17:51:36 -0700

    updated icons & footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer(props) {
   return (
     <div id="footer">
-      <a href="https://www.flaticon.com/free-icons/wiki" title="wiki icons" target="_blank">Wiki icons created by Freepik - Flaticon</a>
+      <a href="https://www.flaticon.com/free-icons/wiki" title="wiki icons" target="_blank" rel="noreferrer">Wiki icons created by Freepik - Flaticon</a>
     </div>
   );
 };
